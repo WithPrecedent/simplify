5b30c390c9aaf32ae72adb84f2c2220acd5b43f6
WithPrecedent <coreyrayburnyung@gmail.com>
2019-10-17 18:25:58 -0400

    test for develop branch
    Signed-off-by: WithPrecedent <coreyrayburnyung@gmail.com>

--- simplify/core/package.py
+++ simplify/core/package.py
@@ -140,6 +140,7 @@ class SimplePackage(SimpleClass):
                 self.steps.update({step: self.options[step]})
         return self
 
+
 @dataclass
 class SimpleComparer(SimpleClass):
     """Parent class for building and controlling iterable steps.
@@ -177,21 +178,21 @@ class SimpleComparer(SimpleClass):
         return self
 
     """ Core siMpLify methods """
-    
+
     def draft(self):
         """ Declares defaults for class."""
         super().draft()
         if getattr(self, self.comparer_iterable) is None:
             setattr(self, self.comparer_iterable, {})
         return self
-    
+
     def edit_comparer(self, comparers):
-        """Adds a comparer or list of comparers to the attribute named in 
+        """Adds a comparer or list of comparers to the attribute named in
         'comparer_iterable'.
 
         Args:
-            comparers(dict(str: SimplePlan or list(dict(str: SimplePlan)): 
-                plans(s) to be added to the attribute named in 
+            comparers(dict(str: SimplePlan or list(dict(str: SimplePlan)):
+                plans(s) to be added to the attribute named in
                 'comparer_iterable'.
         """
         if isinstance(comparers, dict):
@@ -200,7 +201,7 @@ class SimpleComparer(SimpleClass):
             for comparer in comparers:
                 getattr(self, self.comparer_iterable).update(comparers)
         return self
-        
+
     def publish(self):
         super().publish()
         setattr(self.comparer, 'options', self.options)
@@ -215,7 +216,7 @@ class SimpleComparer(SimpleClass):
             setattr(self, step, self.options[step](
                 technique = self.steps[step]))
         return self
-    
+
     def implement(self, *args, **kwargs):
         """Method that implements all of the publishd objects with the passed
         args and kwargs.
@@ -268,10 +269,10 @@ def SimpleSequencer(SimplePackage):
 
     def __post_init__(self):
         super().__post_init__()
-        return self    
+        return self
 
     """ Core siMpLify methods """
-    
+
     def publish(self):
         super().publish()
         for step in self.sequence:
